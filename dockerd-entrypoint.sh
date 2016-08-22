31ed49dbb46baf27b3027ff174ec0b886ef7df49
Mike Lambert <lambert8@illinois.edu>
2016-08-22 15:31:38 +0000

    entrypoint now sets the timeout higher

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$#" -eq 0 -o "${1:0:1}" = '-' ]; then
     set -- docker daemon \
         --host=unix:///var/run/docker.sock \
         --host=tcp://0.0.0.0:2375 \
-        --storage-driver=vfs \
+        --storage-driver=overlay \
         "$@"
 fi
  
@@ -14,9 +14,11 @@ if [ "$1" = 'docker' -a "$2" = 'daemon' ]; then
     # (and we'll run dind explicitly with "sh" since its shebang is /bin/bash)
     set -- sh "$(which dind)" "$@"
 fi
-
-#sudo chmod 666 /var/run/docker.sock
-#docker ps
  
 nohup "$@" </dev/null >dind.log 2>&1 &
-/eclipse-che-4.5.1/bin/che.sh --skip:uid --remote:141.142.208.180 -p:8085
+
+sleep 2s
+
+/bin/sed -i 's/machine.ws_agent.max_start_time_ms=60000/machine.ws_agent.max_start_time_ms=600000/' /eclipse-che-4.5.1/conf/che.properties
+
+/eclipse-che-4.5.1/bin/che.sh --skip:uid -p:8080
