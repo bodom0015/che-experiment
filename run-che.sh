90af9a2fd919e419d37640fc4538ee603b4bba99
Mike Lambert <lambert8@illinois.edu>
2016-08-22 15:06:16 -0500

    Update run-che.sh

--- run-che.sh
+++ run-che.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-docker run -d --net=host \
-  --name=codenvy \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /home/user/che/lib:/home/user/che/lib-copy \
-  -v /home/user/che/workspaces:/home/user/che/workspaces \
-  -v /home/user/che/storage:/home/user/che/storage \
-  codenvy/che $1
+#docker run -d --net=host \
+#  --name=codenvy \
+#  -v /var/run/docker.sock:/var/run/docker.sock \
+#  -v /home/user/che/lib:/home/user/che/lib-copy \
+#  -v /home/user/che/workspaces:/home/user/che/workspaces \
+#  -v /home/user/che/storage:/home/user/che/storage \
+#  codenvy/che $1
+
+
+docker run --name=che -it --rm --net=host --privileged -e CHE_HOST=141.142.208.183 -e CHE_PORT=8080 ndslabs/che:dind
